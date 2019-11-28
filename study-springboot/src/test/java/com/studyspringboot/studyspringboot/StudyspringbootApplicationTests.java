cb15151b76493032a52eeda3b0af1e038cf1f39c
root <root@localhost.localdomain>
2019-11-29 05:45:10 +0800

    springboot

---
+++ study-springboot/src/test/java/com/studyspringboot/studyspringboot/StudyspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyspringboot.studyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
